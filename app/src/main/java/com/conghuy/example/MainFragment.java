1a596206cf1a5cb07837d2a29716deba9d2d45dc
CongHuy <huynhconghuy92@gmail.com>
2018-05-22 17:33:52 +0700

    --

--- app/src/main/java/com/conghuy/example/MainFragment.java
+++ app/src/main/java/com/conghuy/example/MainFragment.java
@@ -1,6 +1,7 @@
 package com.conghuy.example;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import com.conghuy.example.databinding.MainFragmentBinding;
 import javax.inject.Inject;
 
 import dagger.android.support.AndroidSupportInjection;
@@ -16,6 +17,7 @@ import dagger.android.support.AndroidSupportInjection;
 public class MainFragment extends Fragment {
     @Inject
     NetworkApi networkApi;
+    MainFragmentBinding binding;
     @Override
     public void onAttach(Context context) {
         AndroidSupportInjection.inject(this);
@@ -25,28 +27,29 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_main, container, false);
-        initView(v);
-        return v;
+//        View v = inflater.inflate(R.layout.main_fragment, container, false);
+//        initView(v);
+//        return v;
 
-//        ((SettingFragment) getParentFragment()).setCommonInfo(Utils.getMsg(getActivity(), R.string.Smartphone_Link_Debug_screen), true);
-//        binding = DataBindingUtil.inflate(
-//                inflater, R.layout.activity_main, container, false);
-//        View view = binding.getRoot();
-//        //here data must be an instance of the class MarsDataProvider
-//        binding.setLifecycleOwner(this);
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.main_fragment, container, false);
+        View view = binding.getRoot();
+        //here data must be an instance of the class MarsDataProvider
+        binding.setLifecycleOwner(this);
 //        binding.setViewModel(viewModel);
-//
-//        return view;
+
+        initView();
+
+        return view;
 
 //        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 //        binding.setLifecycleOwner(this);
 //        binding.setViewModel(viewModel);
     }
 
-    private void initView(View v) {
+    private void initView() {
         boolean injected = networkApi == null ? false : true;
-        TextView userAvailable = (TextView) v.findViewById(R.id.target);
-        userAvailable.setText("Dependency injection worked: " + String.valueOf(injected));
+//        TextView userAvailable = (TextView) v.findViewById(R.id.target);
+        binding.target.setText("Dependency injection worked: " + String.valueOf(injected));
     }
 }
